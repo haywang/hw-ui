19dad238698d7e26321de207559eea7b86cea239
haywang <hwang@stu.xidian.edu.cn>
2019-04-26 20:55:33 +0800

    lint之后

---
+++ packages/button/src/button.vue
@@ -0,0 +1,25 @@
+<template>
+  <button
+    class="hw-button"
+    @click="handleClick"
+  >
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'HwButton',
+  props: {
+
+  },
+  computed: {
+
+  },
+  methods: {
+    handleClick(evt) {
+      this.$emit('click', evt);
+    }
+  }
+}
+</script>
+
